9374192f97c4690f7d31ef10a29d1b863287bfc3
Colin Pringle-Wood <colin.pringle-wood@aussiecommerce.com.au>
2019-10-02 14:40:49 +1000

    setting app env

--- src/start.ts
+++ src/start.ts
@@ -1,11 +1,14 @@
-require("dotenv-safe").config({
-  path: ".env"
-});
+const appEnv = process.env.APP_ENV || "developement";
+
+if (appEnv !== 'production') {
+  require("dotenv-safe").config({
+    path: ".env"
+  });
+}
 
 import server from "./server";
 
 const port = process.env.PORT || 8080;
-const appEnv = process.env.APP_ENV || "developement";
 
 server().listen(port, () => {
   console.log(`Starting ${appEnv} server on port ${port}`);
