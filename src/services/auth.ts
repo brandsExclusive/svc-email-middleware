207f278918dc4fd97707b228b1812fdfb5337da1
Colin Pringle-Wood <colin.pringle-wood@aussiecommerce.com.au>
2019-11-07 11:06:09 +1100

    removing logging

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -5,14 +5,12 @@ const endpoint = process.env.API_HOST || "api.luxgroup.com";
 
 export async function getUser(requestHeaders: any): Promise<AuthResponse> {
   const url = "http://prod-svc-auth.lescapes.com/current_user";
-  console.log('whats the url', url);
   delete requestHeaders.host;
   requestHeaders.origin = 'https://luxuryescapes.com'
   const options = {
     method: "GET",
     headers: requestHeaders
   };
-  console.log('whats the headers', options);
   try {
     const resp = await fetch(url, options);
     if (resp.status !== 200) {
@@ -20,7 +18,6 @@ export async function getUser(requestHeaders: any): Promise<AuthResponse> {
       return {status: resp.status, user: undefined}
     }
     const authUser = await resp.json();
-    console.log('whats the user', authUser);
     return { status: resp.status, user: authUser };
   } catch (err) {
     console.log('error authenticating', err);
